73a8e49baec671eb1beae194bb1ca127885611a1
xihangzhou <xihang_zhou@163.com>
2022-03-11 10:54:42 +0800

    fix:清除无用代码

--- pages/goods/category/index.js
+++ pages/goods/category/index.js
@@ -1,32 +1,9 @@
 import { getCategoryList } from '../../../services/good/fetchCategoryList';
 Page({
   data: {
-    goodsList: [],
     list: [],
-    show: false,
-    pageSize: 10,
-    pageNum: 1,
-    loadMoreStatus: 0,
-    totalCount: 0,
-    hasLoaded: false,
   },
-  generalQueryData(reset = false) {
-    const { pageNum, pageSize } = this.data;
-
-    const params = {
-      pageNum: 1,
-      pageSize: 30,
-    };
-    // 重置请求
-    if (reset) return params;
-
-    return {
-      ...params,
-      pageNum: pageNum + 1,
-      pageSize,
-    };
-  },
-  async init(reset = true) {
+  async init() {
     try {
       const result = await getCategoryList();
       this.setData({
@@ -40,38 +17,12 @@ Page({
   onShow() {
     this.getTabBar().init();
   },
-  onChange(e) {
+  onChange() {
     wx.navigateTo({
       url: '/pages/goods/list/index',
     });
   },
-  showButton() {
-    this.setData({
-      show: true,
-    });
-  },
-  hideOverlay() {
-    this.setData({
-      show: false,
-    });
-  },
-
-  handleAddCart() {
-    wx.showToast({
-      title: '加入购物车成功',
-    });
-  },
   onLoad() {
     this.init(true);
-    wx.getStorage({
-      key: 'cartNum',
-      success: (res) => {
-        if (res.data) {
-          this.setData({
-            num: res.num,
-          });
-        }
-      },
-    });
   },
 });
