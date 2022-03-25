a7134c845519419bb67e95fd1baf5326b2bf43a0
Winfans <1123642601@qq.com>
2022-03-25 09:14:17 +0800

    fix: 删除log

--- pages/order/components/order-button-bar/index.js
+++ pages/order/components/order-button-bar/index.js
@@ -11,7 +11,6 @@ Component({
       type: Object,
       observer(order) {
         // 判定有传goodsIndex ，则认为是商品button bar, 仅显示申请售后按钮
-        console.log(this.properties?.goodsIndex);
         if (this.properties?.goodsIndex) {
           const goods = order.goodsList[Number(this.properties.goodsIndex)];
           this.setData({
