89de493200bd6a66c698ec48eb0d68164f079828
Winfans <1123642601@qq.com>
2022-03-25 09:37:34 +0800

    fix: 修复订单按钮判空bug

--- pages/order/components/order-button-bar/index.js
+++ pages/order/components/order-button-bar/index.js
@@ -11,10 +11,7 @@ Component({
       type: Object,
       observer(order) {
         // 判定有传goodsIndex ，则认为是商品button bar, 仅显示申请售后按钮
-        if (
-          this.properties?.goodsIndex !== undefined ||
-          this.properties?.goodsIndex !== null
-        ) {
+        if (this.properties?.goodsIndex !== null) {
           const goods = order.goodsList[Number(this.properties.goodsIndex)];
           this.setData({
             buttons: {
