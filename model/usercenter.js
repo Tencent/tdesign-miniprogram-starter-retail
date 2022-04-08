1c81d540c15452603c489a65c782de0c91965b20
oliverppeng <oliverppeng@tencent.com>
2022-04-08 17:22:48 +0800

    feat: 走查UI优化

--- model/usercenter.js
+++ model/usercenter.js
@@ -1,7 +1,7 @@
 const userInfo = {
   avatarUrl:
     'https://we-retail-static-1300977798.cos.ap-guangzhou.myqcloud.com/retail-ui/components-exp/avatar/avatar-1.jpg',
-  nickName: 'Tdesign 🌟',
+  nickName: 'TDesign 🌟',
   phoneNumber: '13438358888',
   gender: 2,
 };
